0a3b7739887e0967fcd4da7217057bcfa0e37e41
dev-library <dev.rary@gmail.com>
2025-08-20 23:27:37 +0900

    chore: remove cloudformation auto config

--- src/main/java/com/example/dynamodbdemo/DynamodbDemoApplication.java
+++ src/main/java/com/example/dynamodbdemo/DynamodbDemoApplication.java
@@ -3,7 +3,10 @@ package com.example.dynamodbdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.cloud.aws.autoconfigure.context.ContextStackAutoConfiguration.class,
+    org.springframework.cloud.aws.autoconfigure.context.ContextRegionProviderAutoConfiguration.class
+})
 public class DynamodbDemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DynamodbDemoApplication.class, args);
